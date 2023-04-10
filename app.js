b736f032fbd15d95b9e35cd9b18d8ddf6084c47c
SENahdi <alnahdi.se@gmail.com>
2023-04-10 11:21:49 -0700

    Adding a Dockerfile

--- app.js
+++ app.js
@@ -119,7 +119,8 @@ Sentry.init({
   tracesSampleRate: 1.0,
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, process.env.IP, function(){
 	console.log("Server is on");
+	console.log(port);
 	});
